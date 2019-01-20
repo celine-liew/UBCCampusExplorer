e16fe9db8e267ef67db279a04c334a1221492c8e
m4j2b <m4j2b@ugrad.cs.ubc.ca>
2019-01-19 23:58:50 -0800

    add lines to performquery

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -22,7 +22,38 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     public performQuery(query: any): Promise <any[]> {
-        return Promise.reject("Not implemented.");
+        const promise: Promise<string[]> = new Promise(function (resolve, reject) {
+            let queryobj: object;
+            try {
+                queryobj = JSON.parse(query);
+            } catch (e) {
+                throw new InsightError("Invalid query string");
+            }
+            let tree: AST;
+            let keys: string[] = [];
+            // this function gets all the keys as an array of queryobj
+            keys = Object.keys(queryobj);
+            // if the queryobj has more than three keys, it must be invalid
+            if (keys.length >= 3) {
+                throw new InsightError("Excess keys in query");
+            } else {
+                if (!queryobj.hasOwnProperty("WHERE")) {
+                    throw new InsightError("Missing Where");
+                } else if (!queryobj.hasOwnProperty("OPTIONS")) {
+                    throw new InsightError("Missing Options");
+                } else if (!(query["OPTIONS"].hasOwnProperty("COLUMNS"))) {
+                    throw new InsightError("Options Missing Columns");
+                } else if (query["OPTIONS"]["COLUMNS"].length <= 0) {
+                    throw new InsightError("Columns must be an un-empty array");
+                } else {
+                    let wherevalid = await InsightFacade.validateWhere(queryobj["WHERE"]);
+                    let optionsvalid = await InsightFacade.validataOptions(queryobj["OPTIONS"]);
+                    // if(where)
+                }
+            }
+
+        });
+        return promise;
     }
 
     public listDatasets(): Promise<InsightDataset[]> {
