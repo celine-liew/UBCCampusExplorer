f782bd530b3a88fb883c5945dbf9496f60067db3
Celine <b0o2b@ugrad.cs.ubc.ca>
2019-01-24 00:18:56 -0800

    WIP add data

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,3 +1,4 @@
+// tslint:disable
 import Log from "../Util";
 import {IInsightFacade, InsightDataset, InsightDatasetKind} from "./IInsightFacade";
 import {InsightError, NotFoundError} from "./IInsightFacade";
@@ -9,24 +10,48 @@ import { PassThrough } from "stream";
  * Method documentation is in IInsightFacade
  *
  */
+export interface IHash {
+    [id: string]: [string];
+}
+
 export default class InsightFacade implements IInsightFacade {
+
+public addHash: IHash = {};
+public unzipContent: string[] = [];
 
     constructor() {
         Log.trace("InsightFacadeImpl::init()");
     }
 
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-        const dataZip = new JSZip (content);
-        if (dataZip.folder(kind)) {
-            // tslint:disable-next-line
+        return JSZip.loadAsync(content, {base64: true}).then(zip => {
+            const files: Promise<string>[] = [];
 
-        }
+            zip.forEach((path, object) => {
+                if (path.startsWith('courses/') && !object.dir) { // check courses folder exist
+                    files.push(object.async("text")); // take files from courses folder only
+                }
+            })
+
+            if (files.length){ // empty lists of files
+                throw new InsightError("No Valid File");
+            }
+            return Promise.all(files);
+        })
+        .then(files => {
+            files.forEach(file => {
+                JSON.parse(file);
+            })
+            return "success";
+            // add only valid json to hash map
+        })
+        .catch(err => {
+            return err;
+        })
+    }
+        // tslint:disable-next-line:align
 
         // { [key: string]: Type; }
-
-        return Promise.reject("Not implemented.");
-    }
-
     public removeDataset(id: string): Promise<string> {
         return Promise.reject("Not implemented.");
     }
