b8a8864395081d673388d07423bcf7efa6d2e973
Celine <b0o2b@ugrad.cs.ubc.ca>
2019-01-24 23:35:28 -0800

    addDatabase checking courseSection valid

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -24,6 +24,7 @@ public unzipContent: string[] = [];
     }
 
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
+        const coursesKeys: string[] = ['Subject', 'Course', 'Avg', 'Professor', 'Title', 'Pass', 'Fail','Audit','id','Year'];
         return JSZip.loadAsync(content, {base64: true}).then(zip => {
             const files: Promise<string>[] = [];
 
@@ -33,20 +34,37 @@ public unzipContent: string[] = [];
                 }
             })
 
-            if (files.length){ // empty lists of files
+            if (!files.length){ // empty lists of files
                 throw new InsightError("No Valid File");
             }
             return Promise.all(files);
         })
         .then(files => {
+            let courseFile;
+            const validCourseSections: Object[] = []
             files.forEach(file => {
-                JSON.parse(file);
+                courseFile = JSON.parse(file); // if valid Json
+                if (courseFile['result'].length >= 1){
+                    courseFile['result'].forEach((cSection : any) => {
+                        const validSection = coursesKeys.every((key) => {
+                            return Object.keys(cSection).includes(key);
+                        })
+                        if (validSection) { // TO CONTINUE...
+
+
+                            console.log("invalid" + cSection['id']);
+                        }
+                    })
+                }
             })
-            return "success";
-            // add only valid json to hash map
         })
         .catch(err => {
+            if (err instanceof Error){
+                throw new InsightError(err)
+            }
             return err;
+        }).catch(err => {
+            return Promise.reject(err);
         })
     }
         // tslint:disable-next-line:align
