796aaa985b541ba870999e00e4aac62593f54269
Celine <b0o2b@ugrad.cs.ubc.ca>
2019-03-19 14:14:42 -0700

    fixed lines

--- test/Server.spec.ts
+++ test/Server.spec.ts
@@ -1,16 +1,11 @@
 import Server from "../src/rest/Server";
-
-
 import TestUtil from "./TestUtil";
 import Log from "../src/Util";
-
 import InsightFacade from "../src/controller/InsightFacade";
 import chai = require("chai");
 import { expect } from "chai";
-
 import chaiHttp = require("chai-http");
 import { InsightDatasetKind } from "../src/controller/IInsightFacade";
-
 
 describe("Facade D3", function () {
 
